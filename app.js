8135cdc97223e52c3b4c8296440ff525e03fb8c6
Lucia Brammer <brammerl96@gmail.com>
2020-03-25 16:09:41 -0700

    Step five in lab upload

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 console.log('Hello World');
 document.body.style.color = 'orange';
 
+
 const monty = {
     species: 'cat',
     color: 'grey-white',
     age: '6 months',
     rescue: true,
     owner: 'lucia'
-};
+    };
 
-const cheese = {
+    const cheese = {
     species: 'cat',
     color: 'orange-white',
     age: '7 months',
     rescue: true,
     owner: 'nicole'
-};
+    };
 
 const lucy = {
     species: 'cat',
@@ -23,31 +24,32 @@ const lucy = {
     age: '2 years',
     rescue: true,
     owner: 'gail'
+    };
 
-};
-
-const pixis = {
+const pixis ={
     species: 'dog',
     color: 'tan',
     age: '10 years',
     rescue: true,
     owner: 'gail'
-};
+    };
 
-const bella = {
+    const bella = {
     species: 'dog',
     color: 'tan',
     age: '8 years',
     rescue: true,
     owner: 'gail'
-};
+    }
 
 const pets = [monty, cheese, lucy, pixis, bella];
+const petNames = ['monty', 'cheese', 'lucy', 'pixis', 'bella']
 
 function logOutThings() {
     console.log('Here are your ', pets.length, ' things!');
-    for (let i = 0; i < pets.length; i++) {
-     console.log(pets[i]);
+    for(let i = 0; i <pets.length; i++){
+        console.log(petNames[i]);
+        console.log(pets[i]);
     };
 };
 
